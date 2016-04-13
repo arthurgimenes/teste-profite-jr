ce8ffc1dc53b6d986c0b2823b2e777c12870184d
Millena Souza <millenabeserrasouza@gmail.com>
2016-04-13 18:22:15 -0300

    Added files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php include "header.php" ?>
+
+<div class="banner-topo">
+	<div class="container">
+		<div id="owl-banner" class="owl-carousel owl-theme">
+		  <div class="item">
+		  	<img src="images/banner1.jpg" alt="A taça é nossa">
+		  </div>
+		  <div class="item">
+		  	<img src="images/banner2.jpg" alt="Criamos experiências de compra fantásticas">
+		  </div>
+		  <div class="item">
+		  	<img src="images/banner3.jpg" alt="Aplicativos">
+		  </div>
+		  <div class="item">
+		  	<img src="images/banner4.jpg" alt="E-commerce">
+		  </div>
+		  <div class="item">
+		  	<img src="images/banner5.jpg" alt="Compra online">
+		  </div>
+		</div>
+	</div>
+</div>
+
+<div class="shelf-1">
+	<div class="container">
+		<div id="owl-shelf-1" class="owl-carousel owl-theme">
+		  <div class="item shoes">
+		  	<a href="images/calçado1_zoom.png" rel="prettyPhoto">
+		  		<img src="images/calçado1_zoom.png" alt="Calçado 1">
+		  	</a>
+			<h2>Sandália Ferracini Ecologic</h2>
+			<input type="number" class="rating" id="rating-stars" name="rating-stars" data-min="1" data-max="5" value="0">
+			<p>Por: R$95,92 ou até 4x de R$ 23,98</p>
+			<a href="#" class="bt-comprar">Comprar</a>
+			<p class="economize">Economize: R$ 19.98</p>
+		  </div>
+		  <div class="item shoes">
+		  	<a href="images/calçado2_zoom.png" rel="prettyPhoto">
+		  		<img src="images/calçado2_zoom.png" alt="Calçado 2">
+		  	</a>
+			<h2>Chinelo Kildare</h2>
+			<input type="number" class="rating" id="rating-stars" name="rating-stars" data-min="1" data-max="5" value="0">
+			<p class="price-before">De: R$ 229,00</p>
+			<p>Por: R$95,92 ou até 4x de R$ 23,98</p>
+			<a href="#" class="bt-comprar">Comprar</a>
+			<p class="economize">Economize: R$ 19.98</p>
+		  </div>
+		  <div class="item shoes">
+		  	<a href="images/calçado3_zoom.png" rel="prettyPhoto">
+		  		<img src="images/calçado3_zoom.png" alt="Calçado 3">
+		  	</a>
+			<h2>Tênis Ferracini Week</h2>
+			<input type="number" class="rating" id="rating-stars" name="rating-stars" data-min="1" data-max="5" value="0">
+			<p class="price-before">De: R$ 229,00</p>
+			<p>Por: R$95,92 ou até 4x de R$ 23,98</p>
+			<a href="#" class="bt-comprar">Comprar</a>
+			<p class="economize">Economize: R$ 19.98</p>
+		  </div>
+		  <div class="item shoes">
+		  	<a href="images/calçado4_zoom.png" rel="prettyPhoto">
+		  		<img src="images/calçado4_zoom.png" alt="Calçado 4">
+		  	</a>
+			<h2>Tênis democrata Fan</h2>
+			<input type="number" class="rating" id="rating-stars" name="rating-stars" data-min="1" data-max="5" value="0">
+			<p class="price-before">De: R$ 229,00</p>
+			<p>Por: R$95,92 ou até 4x de R$ 23,98</p>
+			<a href="#" class="bt-comprar">Comprar</a>
+			<p class="economize">Economize: R$ 19.98</p>
+		  </div>
+		  <div class="item shoes">
+		  	<a href="images/calçado2_zoom.png" rel="prettyPhoto">
+		  		<img src="images/calçado2_zoom.png" alt="Calçado 2">
+		  	</a>
+			<h2>Sandália Ferracini Ecologic</h2>
+			<input type="number" class="rating" id="rating-stars" name="rating-stars" data-min="1" data-max="5" value="0">
+			<p class="price-before">De: R$ 229,00</p>
+			<p>Por: R$95,92 ou até 4x de R$ 23,98</p>
+			<a href="#" class="bt-comprar">Comprar</a>
+			<p class="economize">Economize: R$ 19.98</p>
+		  </div>
+		  <div class="item shoes">
+		  	<a href="images/calçado3_zoom.png" rel="prettyPhoto">
+		  		<img src="images/calçado3_zoom.png" alt="Calçado 3">
+		  	</a>
+			<h2>Tênis democrata Fan</h2>
+			<input type="number" class="rating" id="rating-stars" name="rating-stars" data-min="1" data-max="5" value="0">
+			<p class="price-before">De: R$ 229,00</p>
+			<p>Por: R$95,92 ou até 4x de R$ 23,98</p>
+			<a href="#" class="bt-comprar">Comprar</a>
+			<p class="economize">Economize: R$ 19.98</p>
+		  </div>
+		</div>
+	</div>
+</div>
+
+<?php include "footer.php" ?>
