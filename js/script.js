bbc2f23d7d5d81da399bebbe3f60d7302bededcc
Gabriel Leocadio Almeida da Silva <gabriel.silva73@fatec.sp.gov.br>
2016-04-15 04:31:36 -0300

    Teste no JSON

--- js/script.js
+++ js/script.js
@@ -5,4 +5,32 @@ $(document).ready(function() {
     $('#carousel2').carousel({
         interval: 40000
     });
+    $.getJSON("https://raw.githubusercontent.com/Leocadio94/teste-profite-jr/master/calcados.json", function(data) {
+            var json = JSON.parse(JSON.stringify(data));
+
+            $(json.calcados).each(function() {
+                    var titulo = this.titulo;
+                    var precoOriginal = this.precoOriginal;
+                    var precoAtual = this.precoAtual;
+                    var maxParcelas = this.maxParcelas;
+                    var parcela = this.parcela;
+                    var economia = this.economia;
+                    var imgUrl = this.imgUrl;
+                    var imgHoverUrl = this.imgHoverUrl;
+                    var imgZoomUrl = this.imgZoomUrl;
+
+                    console.log(titulo + " - " + precoOriginal + " - " + precoAtual + " - " +
+                        maxParcelas + " - " + parcela + " - " + economia + " - " +
+                        imgUrl + " - " + imgHoverUrl + " - " + imgZoomUrl);
+                        
+                    $("#carousel2 .carousel-inner").append("<div class='item'>" +
+                            "<div class='well center-block text-center'>" +
+                            "<img class='img-responsive' src='"+imgUrl+"' alt='Foto do "+titulo+"' />" +
+                            titulo + " - " + precoOriginal + " - " + precoAtual + " - " +
+                            maxParcelas + " - " + parcela + " - " + economia +
+                        "</div>" +
+                        "</div>");
+            });
+
+    });
 });
