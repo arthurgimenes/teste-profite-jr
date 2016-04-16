22c953ae29f256ad7fb294efedec564823a4aa20
Gabriel Leocadio Almeida da Silva <gabriel.silva73@fatec.sp.gov.br>
2016-04-15 22:11:15 -0300

    Ajustes no Carousel e Teste do Slick

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+    $('.single-items').slick({
+        infinite: true,
+        speed: 300,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        autoplay: true
+    });
     $('#carousel1').carousel({
         interval: 2000
     });
@@ -6,34 +15,35 @@ $(document).ready(function() {
         interval: 40000
     });
     $.getJSON("https://raw.githubusercontent.com/Leocadio94/teste-profite-jr/master/calcados.json", function(data) {
-            var json = JSON.parse(JSON.stringify(data));
+        var json = JSON.parse(JSON.stringify(data));
 
-            $(json.calcados).each(function() {
-                    var titulo = this.titulo;
-                    var precoOriginal = this.precoOriginal;
-                    var precoAtual = this.precoAtual;
-                    var maxParcelas = this.maxParcelas;
-                    var parcela = this.parcela;
-                    var economia = this.economia;
-                    var imgUrl = this.imgUrl;
-                    var imgHoverUrl = this.imgHoverUrl;
-                    var imgZoomUrl = this.imgZoomUrl;
+        $("#sapatos-item").html("");
 
-                    console.log(titulo + " - " + precoOriginal + " - " + precoAtual + " - " +
-                        maxParcelas + " - " + parcela + " - " + economia + " - " +
-                        imgUrl + " - " + imgHoverUrl + " - " + imgZoomUrl);
+        $(json.calcados).each(function() {
+            var titulo = this.titulo;
+            var precoOriginal = this.precoOriginal;
+            var precoAtual = this.precoAtual;
+            var maxParcelas = this.maxParcelas;
+            var parcela = this.parcela;
+            var economia = this.economia;
+            var imgUrl = this.imgUrl;
+            var imgHoverUrl = this.imgHoverUrl;
+            var imgZoomUrl = this.imgZoomUrl;
 
-                    $("#carousel2 .carousel-inner").append("<div class='item'>" +
-                        "<div class='center-block text-center sapato'>" +
-                        "<img class='img-responsive' src='"+imgHoverUrl+"' alt='Foto do "+titulo+"' />" +
-                        titulo + " - " + precoOriginal + " - " + precoAtual + " - " +
-                        maxParcelas + " - " + parcela + " - " + economia +
-                        "<button type='button' name='button' class='comprar'>"+
-                        "<img class='btn pull-left img-responsive' src='img/sapatos/carrinho.png' alt='Icone de Carrinho' />Comprar" +
-                        "</button>"+
-                        "</div>" +
-                        "</div>");
-            });
+            console.log(titulo + " - " + precoOriginal + " - " + precoAtual + " - " +
+                maxParcelas + " - " + parcela + " - " + economia + " - " +
+                imgUrl + " - " + imgHoverUrl + " - " + imgZoomUrl);
+
+            $("#sapatos-item").append("<div class='center-block text-center col-md-3 col-xs-3 sapato'>" +
+                "<img class='img-responsive' src='" + imgHoverUrl + "' alt='Foto do " + titulo + "' />" +
+                titulo + " - " + precoOriginal + " - " + precoAtual + " - " +
+                maxParcelas + " - " + parcela + " - " + economia +
+                "<button type='button' name='button' class='comprar'>" +
+                "<img class='btn pull-left img-responsive' src='img/sapatos/carrinho.png' alt='Icone de Carrinho' />Comprar" +
+                "</button>" +
+                "</div>" +
+                "</div>");
+        });
 
     });
 });
