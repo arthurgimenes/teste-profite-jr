11a671e96ca365635edba85177d82feeac2fe74d
Gabriel Leocadio Almeida da Silva <gabriel.silva73@fatec.sp.gov.br>
2016-04-16 00:37:05 -0300

    Slick

--- js/script.js
+++ js/script.js
@@ -1,23 +1,12 @@
 $(document).ready(function() {
-    $('.single-items').slick({
-        infinite: true,
-        speed: 300,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        arrows: true,
-        autoplay: true
-    });
     $('#carousel1').carousel({
         interval: 2000
     });
-    $('#carousel2').carousel({
-        interval: 40000
-    });
+
     $.getJSON("https://raw.githubusercontent.com/Leocadio94/teste-profite-jr/master/calcados.json", function(data) {
         var json = JSON.parse(JSON.stringify(data));
 
-        $("#sapatos-item").html("");
+        $(".single-items").html("");
 
         $(json.calcados).each(function() {
             var titulo = this.titulo;
@@ -30,20 +19,47 @@ $(document).ready(function() {
             var imgHoverUrl = this.imgHoverUrl;
             var imgZoomUrl = this.imgZoomUrl;
 
-            console.log(titulo + " - " + precoOriginal + " - " + precoAtual + " - " +
-                maxParcelas + " - " + parcela + " - " + economia + " - " +
-                imgUrl + " - " + imgHoverUrl + " - " + imgZoomUrl);
-
-            $("#sapatos-item").append("<div class='center-block text-center col-md-3 col-xs-3 sapato'>" +
+            $(".single-items").append("<div class='sapato text-center'>" +
                 "<img class='img-responsive' src='" + imgHoverUrl + "' alt='Foto do " + titulo + "' />" +
-                titulo + " - " + precoOriginal + " - " + precoAtual + " - " +
-                maxParcelas + " - " + parcela + " - " + economia +
+                "<p><b>" + titulo + "</b></p>" +
+                "<img class='img-responsive estrelas' src='img/sapatos/estrelas.png' alt='Estrelas' />" +
+                "<p> <strike> De: " + precoOriginal + "</strike></p>" + "<p>Por: " + precoAtual + "</p>" +
+                "<p>" + maxParcelas + "</p>" + "<p>" + parcela + "</p>" +
                 "<button type='button' name='button' class='comprar'>" +
-                "<img class='btn pull-left img-responsive' src='img/sapatos/carrinho.png' alt='Icone de Carrinho' />Comprar" +
+                "<img class='btn pull-left img-responsive' src='img/sapatos/carrinho.png' alt='Icone de Carrinho' />COMPRAR" +
                 "</button>" +
-                "</div>" +
+                "<div class='economia text-center'>Economize " + economia + "</div>" +
                 "</div>");
         });
 
+
+        $('.single-items').slick({
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            autoplay: true,
+            prevArrow: "<img class='a-left control-c prev slick-prev' src='../img/sapatos/seta_esquerda.png'>",
+            nextArrow: "<img class='a-right control-c next slick-next' src='../img/sapatos/seta_direita.png'>",
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
     });
 });
