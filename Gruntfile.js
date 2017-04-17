e6ebafea27bbcce7dca3290bbcb7d2db98151ee8
fernandaquerino <fernandaquerinodelima@hotmail.com>
2017-04-16 22:42:41 -0300

    Inclusão gruntfile e package.json

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function( grunt ) {
+
+  "user strict";
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify : {
+      options : {
+        mangle : false
+      },
+
+      my_target : {
+        files : {
+          'assets/js/main.js' : [ 'assets/_js/script.js']
+        }
+      }
+    }, // uglify
+
+    imagemin: {
+      static:{
+        files: [{
+          expand: true,
+          cwd: 'assets/img',
+          src: ['**/*.{png, jpg, gif}'],
+          dest: 'assets/img'
+        }]
+      }
+    }, //imagemin
+
+    sass : {
+      dist : {
+        options : { style : 'compressed' },
+        files : {
+          'assets/css/style.css' : 'assets/scss/style.scss'
+        }
+      }
+    }, // sass
+
+    watch : {
+      dist : {
+        files : [
+          'assets/_js/**/*',
+          'assets/img/**/*'
+        ],
+
+        tasks : [ 'uglify', 'imagemin', 'sass' ]
+      }
+    } // watch
+
+  });
+
+
+  // Plugins do Grunt
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
+  grunt.loadNpmTasks( 'grunt-contrib-sass' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+  // Tarefas que serão executadas
+  grunt.registerTask( 'default', [ 'uglify', 'imagemin', 'sass'] );
+
+  //Tarefa Watch
+  grunt.registerTask( 'w', [ 'watch' ] );
+
+};
