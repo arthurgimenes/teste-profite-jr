673e3bf99a49aecee4c58ed7f9ff34e184973715
Rafael Melo <rafaelsantosmello@gmail.com>
2017-07-12 01:53:15 -0300

    entrega do teste

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,21 +1,43 @@
-$('.slide_destaque .slide-wrap').cycle({
-    speed: 600,
-    manualSpeed: 100,
-    fx: 'fadeout',
-    manualFx: 'fadeout',
-    slides: 'picture',
-    prev: '.sd-arrow_prev',
-    next: '.sd-arrow_next',
-    pager: '.sd-pager'
+// slide destaque
+$('.slide_destaque .slide-wrap').slick({
+  dots: true,
+  infinite: false,
+  fade: true,
+  prevArrow: '<div class="sd-arrow_prev"></div>',
+  nextArrow: '<div class="sd-arrow_next"></div>',
+  dotsClass: 'sd-pager'
 });
 
-$('.slide_produtos .slide-wrap').cycle({
-    speed: 600,
-    manualSpeed: 100,
-    fx: 'fadeout',
-    manualFx: 'fadeout',
-    slides: '.sp-wrap_produtos',
-    prev: '.sp-arrow_prev',
-    next: '.sp-arrow_next',
-    pager: '.sp-pager'
+
+//slide produtos
+$('.slide_produto .slide-wrap').slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  prevArrow: '<div class="sp-arrow_prev"></div>',
+  nextArrow: '<div class="sp-arrow_next"></div>',
+  responsive: [
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
+});
+
+
+//lightbox zoom
+$('.pd-zoom_produto').magnificPopup({
+  type:'image'
+
 });
