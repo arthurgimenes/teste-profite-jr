b6730dc4f24a6856b0a8bed31acd769e8616e957
Thyago Rosa <thyago.abfc@gmail.com>
2017-09-17 22:35:24 -0300

    Teste - Completo + Servidor Node.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var app = express(),
 app.set('view engine', 'html');
 
 app.use(express.static(__dirname + '/public'));
-
 app.use('/js', express.static(__dirname + '/js/bootstrap'));
 app.use('/js', express.static(__dirname + '/js/jquery'));
 app.use('/js', express.static(__dirname + '/js/popper'));
@@ -18,7 +17,10 @@ app.use('/css', express.static(__dirname + '/css'));
 app.use('/img', express.static(__dirname + '/resources/img/topo'));
 app.use('/img', express.static(__dirname + '/resources/img/banner'));
 app.use('/img', express.static(__dirname + '/resources/img/footer'));
+app.use('/img', express.static(__dirname + '/resources/img/sapatos'));
+app.use('/json', express.static(__dirname + '/resources/json'));
 
+//Funções para ler o json 
 var readJsonFileSync = (filepath, encoding) => {
     if (typeof (encoding) == 'undefined'){
         encoding = 'utf8';
@@ -32,18 +34,19 @@ var getJSON = (file) => {
     return readJsonFileSync(filepath);
 }
 
-json = getJSON('calcados.json');
+json = getJSON('resources/json/calcados.json');
 
+//Configuração de roteamento
 app.get('/', (req, res) => {
     res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
     res.render('index.html');
 })
 
 app.get('/calcados.json', (req, res) => {
-    //res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
     res.send(json);
 });
 
+//Assim que o server.js for executado, ele será executado na porta 3000
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
