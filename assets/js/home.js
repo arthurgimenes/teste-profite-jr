5d8b56633a06a017fd36744e6d09ba871fea6f46
Glayton de Paula <glayton.paula95@gmail.com>
2017-11-27 19:39:24 -0200

    layout responsivo loja

--- assets/js/home.js
+++ assets/js/home.js
@@ -11,10 +11,36 @@ $('.slide').slick({
 });
 
 $('.slide-produtos').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
   slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 
 $('.carousel.carousel-slider').carousel({fullWidth: true});
