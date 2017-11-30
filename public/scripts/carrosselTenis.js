0accc34e1f5dadc39cdf2bd90b282cb5ca534869
CamilaNuttoc <camila.canuto@outlook.com>
2017-11-30 15:24:01 -0200

    gera dados json

--- public/scripts/carrosselTenis.js
+++ public/scripts/carrosselTenis.js
@@ -1,8 +1,37 @@
-$(document).ready(function(){
-    var produto = $('.carrosselnw .content');
-    for(var i=0; i<11; i++){
-        produto.clone().appendTo('.carrosselnw');
+//Gera itens do carrossel
+function seedCarrossel(arr){
+    var produtosJson = JSON.stringify(arr);
+    var produtos = JSON.parse(produtosJson);
+    var carrosselHtml = '';
+    //Constrói itens
+    for(var i=0; i<produtos.length; i++){
+        carrosselHtml += '<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 content">';
+        carrosselHtml += '<div class="thumbnail"><a>';
+        carrosselHtml += '<img src="'+produtos[i].img+'" alt="'
+                       +produtos[i].nome+'" data-toggle="modal" data-target="#tenis"/>';
+        carrosselHtml += '<div class="caption">';
+        carrosselHtml += '<h4>'+produtos[i].nome+'</h4>';
+        carrosselHtml += '<img src="./public/sapatos/estrelas.png"/><br />';
+        carrosselHtml += '<span class="preco-original"><i>De: R$ <span>'
+                         +produtos[i].precoOriginal.toString().replace(".", ",")+'</span></i></span><br />';
+        carrosselHtml += '<p class="preco-desconto"><b>Por: <i>R$ <span>'
+                         +produtos[i].precoPromocao.toString().replace(".", ",")+'</span></i></b></p>';
+        carrosselHtml += '<p class="preco-parcelado"><i>ou <b>até 3X</b> de <b>R$ '
+                         +(produtos[i].precoPromocao/3).toFixed(2).toString().replace(".", ",")+'</b></i></p>';
+        carrosselHtml += '<button class="comprarBtn"><img src="./public/sapatos/carrinho.png" alt="icone carrinho">COMPRAR</button><br />';
+        carrosselHtml += '<span class="preco-economia">Economize: R$ '
+                         +((produtos[i].precoOriginal)-(produtos[i].precoPromocao)).toFixed(2).toString().replace(".", ",")+'</span>';
+        carrosselHtml += '</div></a></div></div>';
     }
+    //Adiciona itens a html
+    $(".carrosselnw").html(carrosselHtml);
+}
+
+//Muda imagem de modal ao clicar
+$('.thumbnail > a img').on('click', function() {
+  var image = $(this).attr("src");
+  image = image.replace('hover', 'zoom');
+  $('.modal-body > img').attr("src", image);
 });
 
 //Inicializa posição de carrossel
