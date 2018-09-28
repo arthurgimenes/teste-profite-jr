fc0739891ead4ea6259273b031a0f0e2a1aa1f8f
Caique Othon <40042581+caiqueotn@users.noreply.github.com>
2018-09-28 04:02:52 -0300

    Adicionando arquivos do teste.
    Arquivos adicionados.

---
+++ js/media-queries.js
@@ -0,0 +1,9 @@
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        document.body.style.backgroundColor = "yellow";
+    } else {
+        document.body.style.backgroundColor = "pink";
+    }
+}
+
+var x = window.matchMedia("(max-width: 100px)")
