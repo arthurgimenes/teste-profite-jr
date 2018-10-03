5bdebe14e9e9416bca1a36534cbe9be15ba27a4c
Armenio Maurício Ferreira Neto <dehme.armenio@gmail.com>
2018-10-03 14:06:17 -0300

    Create react project + containers with vh

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="divApp" className="App">
+        <div id="containerHead" class="container-fluid">
+
+        </div>
+        <div id="containerBanner" class="container-fluid">
+
+        </div>
+        <div id="containerProducts" class="container-fluid">
+
+        </div>
+        <div id="containerFoot" class="container-fluid">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
