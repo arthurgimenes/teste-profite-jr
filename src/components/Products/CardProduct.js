6cdd8a8305aa0d517cbd990db41baaa88028e733
Armenio Maurício Ferreira Neto <dehme.armenio@gmail.com>
2018-10-05 12:36:08 -0300

    product logic add

--- src/components/Products/CardProduct.js
+++ src/components/Products/CardProduct.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import Product1 from "../../assets/product1.png"
+import empytStar from "../../assets/empyt_star.png"
+import fullStar from "../../assets/full_star.png"
+
 
 
 class CardProduct extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            star: fullStar,
+            star2: empytStar,
+            star3: empytStar,
+            star4: empytStar,
+            star5: empytStar,
+        }
+    }
+
+
     render() {
+
         return (
             <div class="card" id="divCardProduct" >
                 <img class="card-img-top" src={Product1} alt="productFoo" />
-                <div class="card-body">
-                    <h5 class="card-title">TÊNIS COURO PUMA R698 Q4 V2</h5>
+                <div id="dicCardBody" class="card-body">
+                    <h5 class="card-title">TÊNIS COURO PUMA R698 {this.props.productId}</h5>
+                    <div class="col-sm">
+                        <img class="card-img-top" src={this.state.star} alt="empytStar" />
+                        <img class="card-img-top" src={this.state.star2} alt="empytStar" />
+                        <img class="card-img-top" src={this.state.star3} alt="empytStar" />
+                        <img class="card-img-top" src={this.state.star4} alt="empytStar" />
+                        <img class="card-img-top" src={this.state.star5} alt="empytStar" />
+                    </div>
                     <p class="card-text">de R$ 299,00</p>
                     <p class="card-text">por R$ 399,00</p>
                     <p class="card-text">ou em 3x de R$ 133,00</p>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,26 +4,47 @@ import CardProduct from "./CardProduct";
 
 
 class Products extends React.Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            productId: 1,
+        }
+        this.beforeClick = this.beforeClick.bind(this)
+        this.afterClick = this.afterClick.bind(this)
+    }
+
+    beforeClick(){
+        this.setState({
+            productId: this.state.productId-1,
+        });
+    }
+
+    afterClick(){
+        this.setState({
+            productId: this.state.productId+1,
+        });
+    }
+
     render() {
         return (
             <div class="row align-items-center">
                 <div id="divBeforeButton" class="col-1">
-                    <span />
+                    <span onClick={this.beforeClick} />
                 </div>
                 <div class="col-sm">
-                    <CardProduct />
+                    <CardProduct productId={this.state.productId}/>
                 </div>
                 <div class="col-sm">
-                    <CardProduct />
+                    <CardProduct productId={this.state.productId+1}/>
                 </div>
                 <div class="col-sm">
-                    <CardProduct />
+                    <CardProduct productId={this.state.productId+2}/>
                 </div>
                 <div class="col-sm">
-                    <CardProduct />
+                    <CardProduct productId={this.state.productId+3}/>
                 </div>
                 <div id="divAfterButton" class="col-1">
-                    <span />
+                    <span onClick={this.afterClick} />
                 </div>
             </div>
         );
