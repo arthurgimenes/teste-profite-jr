6e43efe2e2923861bcd8da07be4ada018b2d4639
Fábio Castro <fabiocastrosani@icloud.com>
2018-10-08 10:40:32 -0300

    First commit

--- app.js
+++ app.js
@@ -27,22 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(process.env.PORT || "3000", function () {});
 
 module.exports = app;
