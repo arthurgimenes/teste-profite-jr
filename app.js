49657856a3500adf620c845c333f5e9e50d45b9c
renan-dev <renanprch@gmail.com>
2019-11-11 01:47:51 -0300

    Teste finalizado

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = require('connect')()
 var serveStatic = require('serve-static');
 
 app.use('/', serveStatic('assets', {'index': ['index.html', 'index.html']}))
-app.listen(8080, function() {
+app.listen(8080, () => {
     console.log('Acesse: http://localhost:8080')
 });
     
