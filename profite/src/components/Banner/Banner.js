9ebbaad9f596f09ad2d216f3ad82bc860058544b
hiago santos <foto.hiago@gmail.com>
2019-11-23 11:44:48 -0300

    criar carrosel automatico

--- profite/src/components/Banner/Banner.js
+++ profite/src/components/Banner/Banner.js
@@ -1,24 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Banner.scss'
 import backgroundImage from '../../img/background-image.png'
+import { Gallery, GalleryImage } from "react-gesture-gallery"
 
+const images = [ backgroundImage, backgroundImage, backgroundImage, backgroundImage]
 
 const Banner = () => {
+
+    const [index, setIndex] = useState(0) 
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (index === 3){
+                setIndex(0)
+            }else{
+                setIndex(prev => prev + 1)
+            }
+        }, 3000)
+        return () => clearInterval(timer)
+    }, [index])
+
     return (
-        <div style={{backgroundImage: `url(${backgroundImage})`}} className="Banner">
-            <header>
-            <h1>Nossa especialidade: experiência de compra.</h1>
-            <div></div>
-            <section>
-                <article>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </article>
-            </section>
-            </header>
-        </div>
+      <div className="Banner">
+          <div className="risco"></div>
+          <h1>Nossa especialidade: experiência de compra.</h1>
+          <Gallery
+          index={index}
+          onRequestChange={i => {
+            setIndex(i);
+          }}
+        >
+          {images.map(image => (
+            <GalleryImage objectFit="contain" key={image} src={image} />
+            ))}
+          
+        </Gallery>
+      </div>
+    
     )
 }
 
